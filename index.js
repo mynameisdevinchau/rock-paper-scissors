464c2cda79d9417608889e023a7a44d09645ce1d
mynameisdevinchau <chau.devin031602@gmail.com>
2025-07-02 03:25:18 -0700

    added getComputerChoice() implementation

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function getComputerChoice(){
     
-
+    let randomNumber = Math.random();
+    let move = "rock";
+    console.log(randomNumber);
+    if(randomNumber < 1/3){
+        move = "paper";
+    } else if(randomNumber < 2/3){
+        move = "scissors";
+    }
 
     return move; 
 }
+console.log(getComputerChoice());
 
-function getHumanChoice(){
-    let result = prompt("type something");
-    return result;
-}
+
+// function getHumanChoice(){
+//     let result = prompt("type something");
+//     return result;
+// }
